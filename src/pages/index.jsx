a6d38d98c321eae00567c19232a2af4a62c4c1ee
Jogi <stigmahotspot@gmail.com>
2022-07-20 17:34:00 +0700

    Admin kita

--- src/pages/index.jsx
+++
@@ -1,7 +0,0 @@
-export { default as Home } from './Home';
-export { default as Intepretasi_Data_Maps } from './Intepretasi_Data_Maps';
-export { default as Intepretasi_Data_Tabel } from './Intepretasi_Data_Tabel';
-export { default as Profil } from './Profil';
-export { default as Berita } from './Berita';
-
-
